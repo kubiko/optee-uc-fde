04daf440a4b2181bab1a8e070f7473817f63ea39
Ondrej Kubik <ondrej.kubik@canonical.com>
2026-06-12 22:27:50 +0100

    ta: add asymmetric key support
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- ta/fde_key_handler/fde_key_handler_ta_entry.c
+++ ta/fde_key_handler/fde_key_handler_ta_entry.c
@@ -3,7 +3,6 @@
  * Copyright 2025, Canonical Ltd.
  */
 
-#include <tee_internal_api.h>
 #include <trace.h>
 
 #include "fde_key_handler_ta_type.h"
@@ -12,9 +11,9 @@
 // TA lock status
 static int _ta_lock;
 
-static TEE_Result lock_ta( uint32_t param_types,
+static TEE_Result lock_ta( uint32_t paramTypes,
                            TEE_Param params[TEE_NUM_PARAMS]);
-static TEE_Result get_ta_lock( uint32_t param_types,
+static TEE_Result get_ta_lock( uint32_t paramTypes,
                                TEE_Param params[TEE_NUM_PARAMS]);
 
 
@@ -26,30 +25,48 @@ TEE_Result TA_CreateEntryPoint(void) {
 
 TEE_Result TA_OpenSessionEntryPoint( uint32_t paramTypes,
                                TEE_Param __maybe_unused params[TEE_NUM_PARAMS],
-                               void __maybe_unused **sessionContext) {
+                               void **session_context) {
 
+    session_ctx_t *ctx;
     DMSG("fde_key_handler: TA_OpenSessionEntryPoint\n");
-    UNUSED(paramTypes);
-    UNUSED(params);
-    UNUSED(sessionContext);
+    (void)paramTypes;
+    ctx = TEE_Malloc(sizeof(*ctx), TEE_MALLOC_FILL_ZERO);
+    if (!ctx) return TEE_ERROR_OUT_OF_MEMORY;
+
+    ctx->key_pair  = TEE_HANDLE_NULL;
+    ctx->key_ready = false;
+
+    *session_context = ctx;
+    DMSG("fde_key_handler: session opened");
     return TEE_SUCCESS;
 }
 
-void TA_CloseSessionEntryPoint( void __maybe_unused *session_context) {
+void TA_CloseSessionEntryPoint( void *session_context) {
+
+    session_ctx_t *ctx;
     DMSG("fde_key_handler: TA_CloseSessionEntryPoint\n");
-    UNUSED(session_context);
+    ctx = (session_ctx_t *)session_context;
+    if (ctx) {
+        if (ctx->key_pair != TEE_HANDLE_NULL)
+            TEE_FreeTransientObject(ctx->key_pair);
+        TEE_Free(ctx);
+    }
+    DMSG("fde_key_handler: session closed – key material zeroised");
 }
 
 void TA_DestroyEntryPoint(void) {
     DMSG("fde_key_handler: TA_DestroyEntryPoint\n");
 }
 
-TEE_Result TA_InvokeCommandEntryPoint( void __maybe_unused *session_context,
+TEE_Result TA_InvokeCommandEntryPoint( void *session_context,
                                        uint32_t cmd_id,
                                        uint32_t paramTypes,
                                        TEE_Param params[TEE_NUM_PARAMS]) {
 
+    session_ctx_t *ctx;
     DLOG("FDE cmd_id = %#"PRIx32"\n", cmd_id);
+    ctx = (session_ctx_t *)session_context;
+
     switch (cmd_id) {
         case TA_CMD_KEY_ENCRYPT:
             return cmd_symmetric_key_crypto(TEE_MODE_ENCRYPT, paramTypes, params);
@@ -59,7 +76,7 @@ TEE_Result TA_InvokeCommandEntryPoint( void __maybe_unused *session_context,
                 EMSG("fde_key_handler: TA is locked for further decrypt oprerations!!");
                 return TEE_ERROR_ACCESS_DENIED;
             }
-            return key_crypto(TEE_MODE_DECRYPT, paramTypes, params);
+            return cmd_symmetric_key_crypto(TEE_MODE_DECRYPT, paramTypes, params);
         case TA_CMD_LOCK:
             return lock_ta(paramTypes, params);
         case TA_CMD_GET_LOCK:
@@ -68,6 +85,15 @@ TEE_Result TA_InvokeCommandEntryPoint( void __maybe_unused *session_context,
             return generate_random(paramTypes, params);
         case TA_CMD_TA_VERSION:
             return get_ta_version(paramTypes, params);
+        /* Asynchronous crypto functions */
+        case TA_CMD_ASYMMETRIC_DERIVE_KEYPAIR:
+            return cmd_asymmetric_derive_keypair(ctx, paramTypes, params);
+        case TA_CMD_ASYMMETRIC_SIGN:
+            return cmd_asymmetric_sign(ctx, paramTypes, params);
+case TA_CMD_ASYMMETRIC_DECRYPT:
+            return cmd_asymmetric_decrypt(ctx, paramTypes, params);
+        case TA_CMD_ASYMMETRIC_GET_PUBKEY:
+            return cmd_asymmetric_get_pubkey(ctx, paramTypes, params);
         default:
             EMSG("fde_key_handler: Command ID %#"PRIx32" is not supported", cmd_id);
             return TEE_ERROR_NOT_SUPPORTED;
--- ta/fde_key_handler/fde_key_handler_ta_handle.h
+++ ta/fde_key_handler/fde_key_handler_ta_handle.h
@@ -6,8 +6,22 @@
 #ifndef FDE_KEY_HANDLER_TA_HANDLE_H_
 #define FDE_KEY_HANDLER_TA_HANDLE_H_
 
+#include <tee_internal_api.h>
+
 #define TA_UNLOCKED  0
 #define TA_LOCKED    1
+
+/* =========================================================================
+ * Session context
+ * ========================================================================= */
+
+typedef struct {
+    TEE_ObjectHandle  key_pair;     /* transient keypair – NULL until derived */
+    uint32_t          algo;         /* ALGO_RSA | ALGO_ECDSA                  */
+    uint32_t          key_bits;     /* e.g. 2048, 256                         */
+    bool              key_ready;
+} session_ctx_t;
+
 
 extern TEE_Result cmd_symmetric_key_crypto(TEE_OperationMode mode,
                              unsigned int paramTypes,
@@ -16,4 +30,19 @@ extern TEE_Result generate_random(uint32_t types,
                                   TEE_Param params[TEE_NUM_PARAMS]);
 extern TEE_Result get_ta_version(uint32_t types,
                                   TEE_Param params[TEE_NUM_PARAMS]);
+
+/* Asymetric crypto operations */
+extern TEE_Result cmd_asymmetric_derive_keypair(session_ctx_t *ctx,
+                                               uint32_t param_types,
+                                               TEE_Param params[4]);
+extern TEE_Result cmd_asymmetric_sign(session_ctx_t *ctx,
+                              uint32_t param_types,
+                              TEE_Param params[4]);
+
+extern TEE_Result cmd_asymmetric_decrypt(session_ctx_t *ctx,
+                                 uint32_t param_types,
+                                 TEE_Param params[4]);
+extern TEE_Result cmd_asymmetric_get_pubkey(session_ctx_t *ctx,
+                                    uint32_t param_types,
+                                    TEE_Param params[4]);
 #endif
